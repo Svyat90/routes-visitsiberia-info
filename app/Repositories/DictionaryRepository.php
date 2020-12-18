07e4324f950c949921dfe07fd0365c0eff383ec4
Svyat Dev <matrix.yurinets.sv@gmail.com>
2020-12-18 18:44:21 +0200

    hot fix

--- app/Repositories/DictionaryRepository.php
+++ app/Repositories/DictionaryRepository.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Collection;
 
-class DictionaryRepository extends Model
+class DictionaryRepository extends Dictionary
 {
 
     /**
